8c916ef3f2d244d3b4040f991c2073ca1b760d35
David T <desarrolladorweb22@gmail.com>
2021-12-12 15:33:10 +0100

    first commit

---
+++ assets/scripts/navegacion.js
@@ -0,0 +1,30 @@
+$('#galeria').click((e)=> {
+    e.preventDefault()
+    swal({
+        title:"Existen dos variantes de Galeria",
+        buttons: {
+            instagram: {
+                text: "Instagram",
+                value: "instagram",
+            },
+            grid: {
+                text: "CSS Grid",
+                value: "grid",
+            },
+        },
+    }).then((value) => {
+        switch (value) {
+        
+            case "instagram":
+                window.location = "galeria_instagram.html"
+            break;
+        
+            case "grid":
+                window.location = "galeria_grid.html"
+            break;
+        
+            default:
+                // Se queda en la misma página
+        }
+    });
+})
